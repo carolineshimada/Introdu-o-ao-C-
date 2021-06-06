1a251dc4a4360891b5da29f9b09171751acb2e14
carolineshimada <71412867+carolineshimada@users.noreply.github.com>
2021-06-05 22:25:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Projeto_em_c_
+{
+    class Program
+    {
+        static void Main(string[] args){
+            double x, y, media;
+            Console.Write("digite primeiro numero");
+            x=double.Parse(Console.ReadLine());
+            Console.Write("digite o segundo numero");
+            y=double.Parse(Console.ReadLine());
+            media = (x + y) / 2.0;
+            Console.WriteLine("Media = "+ media);
+        }
+    }
+}
+
---
+++ anotacoes.md
@@ -0,0 +1,7 @@
+Console.WriteLine("How old are you?");// para imprimir - equivale console.log JS
+$ dotnet run pra rodar no terminal
+  string input = Console.ReadLine();//The word input represents a variable 
+
+dotnet new console// para abrir novo arquivo de projeto
+
+qual a diferença entre     Console.WriteLine("How old are you?"); Console.ReadLine();
